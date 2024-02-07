7879b36ec17a18596d57832b657a06101e351fba
Gris86 <ann.gevorgyan@gmail.com>
2024-02-07 17:42:23 +0300

    commit

---
+++ src/main/clients/Bird.java
@@ -0,0 +1,7 @@
+package main.clients;
+
+public class Bird extends Animal {
+    @Override
+    public void swim() {}
+    // птица умеет ходить, если она не летает
+}
---
+++ src/main/clients/Fish.java
@@ -0,0 +1,9 @@
+package main.clients;
+
+public class Fish extends Animal {
+    @Override
+    public void fly() {}
+
+    @Override
+    public void toGo() {}
+}
